ffbbcf58d48ab9e68a3096082e53198fa287cf31
Szczepan Cieślik <szczepan.cieslik@gmail.com>
2014-09-14 21:46:26 +0200

    Added field machine.

--- super_state_machine/extras.py
+++ super_state_machine/extras.py
@@ -1,5 +1,7 @@
 """Extra utilities for state machines, to make them more usable."""
 
+import six
+
 
 class ProxyString(str):
 
@@ -51,3 +53,79 @@ class PropertyMachine(object):
             actual_state = ''
 
         return ProxyString(actual_state, machine)
+
+
+class FieldMachine(object):
+
+    """Field machine.
+
+    This class is designed for cases when you define field in model, which
+    metaclass does some 'magic' with that - so it is required that this field
+    is present during construction of it in its metaclass. ``FieldMachine``
+    wraps that model and hides its own instances during that construction, so
+    magic can happen, and later replaces properties with instances of
+    :class:`PropertyMachine` with declared machine type.
+
+    Field machine usage is similar to :class:`PropertyMachine` but you must
+    remember that class that contains ``FieldMachine`` properties MUST inherit
+    from ``FieldMachine.wrap(original_model)``.
+
+    Example:
+
+        >>> class SomeModel(FieldMachine.wrap(OriginalBaseModel)):
+        ...
+        ...     field1 = SomeField('field1')
+        ...     field2 = SomeField('field2')
+        ...     field3 = FieldMachine(SomeField('field1'), Machine)
+
+    In case above all magic done by ``OriginalBaseModel`` metaclass will
+    happen, but in instance of ``SomeModel`` property ``field3`` will behave as
+    it was initialized with :class:`PropertyMachine`.
+
+    """
+
+    def __init__(self, field, machine_type):
+        """Init field with replaced field and desired machine type.
+
+        :param field: Some arbitrary field, that must be present for original
+            metaclass.
+        :param machine_type: Machine type that is to be used as property.
+
+        """
+        self.field = field
+        self.machine_type = machine_type
+
+    @classmethod
+    def wrap(cls, wrapped_type):
+        """Wrap type metaclass, to hide field machine for initialization.
+
+        Generated temporary class will hide instances of ``FieldMachine``,
+        execute metaclass of ``wrapped_type`` and replace properties with
+        :class:`PropertyMachine` initialized with ``machine_type`` in
+        ``__init__``.
+
+        :param wrapped_type: Some arbitrary type to wrap.
+
+        """
+        wrapped_type = type(wrapped_type)
+        field_cls = cls
+
+        class TemporaryMetaclass(type):
+
+            def __new__(cls, name, bases, attrs):
+                tmp = {}
+                for name, value in attrs.items():
+                    if isinstance(value, field_cls):
+                        tmp[name] = value
+                        attrs[name] = value.field
+
+                new_class = wrapped_type.__new__(
+                    wrapped_type, name, bases, attrs)
+
+                for name, value in tmp.items():
+                    setattr(
+                        new_class, name, PropertyMachine(value.machine_type))
+
+                return new_class
+
+        return six.with_metaclass(TemporaryMetaclass)
--- tests/test_extras.py
+++ tests/test_extras.py
@@ -1,6 +1,8 @@
 import enum
 import unittest
 
+import six
+
 from super_state_machine import extras, machines
 
 
@@ -42,3 +44,69 @@ class TestPropertyMachine(unittest.TestCase):
         door.lock2.open()
         self.assertEqual(door.lock1, 'open')
         self.assertEqual(door.lock2, 'open')
+
+
+class TestFieldMachine(unittest.TestCase):
+
+    def test_model(self):
+
+        class Model(_Model):
+
+            field1 = _Field('field1')
+            field2 = _Field('field2')
+            field3 = _Field('field3')
+
+        model = Model()
+        self.assertEqual(3, len(model.fields))
+        for name, field in model.fields.items():
+            self.assertIs(True, isinstance(field, _Field))
+            self.assertEqual(name, field.name)
+
+    def test_field_machine(self):
+
+        class Model(extras.FieldMachine.wrap(_Model)):
+
+            field1 = _Field('field1')
+            field2 = _Field('field2')
+            field3 = extras.FieldMachine(_Field('field3'), Lock)
+
+        model = Model()
+        self.assertEqual(3, len(model.fields))
+        for name, field in model.fields.items():
+            self.assertIs(True, isinstance(field, _Field))
+            self.assertEqual(name, field.name)
+
+        self.assertEqual(model.field3, 'open')
+        model.field3.lock()
+        self.assertEqual(model.field3, 'locked')
+        model.field3.open()
+        self.assertEqual(model.field3, 'open')
+
+
+class _Field(object):
+
+    def __init__(self, name):
+        self.name = name
+
+
+class _ModelMetaclass(type):
+
+    def __new__(cls, name, bases, attrs):
+        fields = {}
+        to_delete = []
+        for name, attr in attrs.items():
+            if isinstance(attr, _Field):
+                fields[name] = attr
+                to_delete.append(attrs[name])
+
+        attrs = {
+            key: value for key, value in attrs.items() if key not in to_delete}
+
+        attrs['fields'] = fields
+
+        return super(cls, cls).__new__(cls, name, bases, attrs)
+
+
+class _Model(six.with_metaclass(_ModelMetaclass)):
+
+    pass
