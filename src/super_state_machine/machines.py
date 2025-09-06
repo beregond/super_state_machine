0dd04617276f51b9b04ba69ba794ba4ce1c5cc85
Szczepan Cieślik <szczepan.cieslik@gmail.com>
2025-09-06 16:40:21 +0200

    Remove six

--- src/super_state_machine/machines.py
+++ src/super_state_machine/machines.py
@@ -3,8 +3,6 @@
 from enum import Enum
 from functools import partial
 
-import six
-
 from . import utils
 
 
@@ -12,14 +10,12 @@ NotSet = object()
 
 
 class DefaultMeta(object):
-
     """Default configuration values."""
 
-    states_enum_name = 'States'
+    states_enum_name = "States"
 
 
 class AttributeDict(dict):
-
     def __setattr__(self, key, value):
         self[key] = value
 
@@ -28,7 +24,6 @@ class AttributeDict(dict):
 
 
 class StateMachineMetaclass(type):
-
     """Metaclass for state machine, to build all its logic."""
 
     def __new__(cls, name, bases, attrs):
@@ -71,12 +66,13 @@ class StateMachineMetaclass(type):
     @classmethod
     def _check_states_enum(cls):
         """Check if states enum exists and is proper one."""
-        states_enum_name = cls.context.get_config('states_enum_name')
+        states_enum_name = cls.context.get_config("states_enum_name")
         try:
-            cls.context['states_enum'] = getattr(
-                cls.context.new_class, states_enum_name)
+            cls.context["states_enum"] = getattr(
+                cls.context.new_class, states_enum_name
+            )
         except AttributeError:
-            raise ValueError('No states enum given!')
+            raise ValueError("No states enum given!")
 
         proper = True
         try:
@@ -86,17 +82,17 @@ class StateMachineMetaclass(type):
             proper = False
 
         if not proper:
-            raise ValueError(
-                'Please provide enum instance to define available states.')
+            raise ValueError("Please provide enum instance to define available states.")
 
     @classmethod
     def _check_if_states_are_strings(cls):
         """Check if all states are strings."""
         for item in list(cls.context.states_enum):
-            if not isinstance(item.value, six.string_types):
+            if not isinstance(item.value, str):
                 raise ValueError(
-                    'Item {name} is not string. Only strings are allowed.'
-                    .format(name=item.name)
+                    "Item {name} is not string. Only strings are allowed.".format(
+                        name=item.name
+                    )
                 )
 
     @classmethod
@@ -105,7 +101,7 @@ class StateMachineMetaclass(type):
 
         Initial state is required.
         """
-        state_value = cls.context.get_config('initial_state', None)
+        state_value = cls.context.get_config("initial_state", None)
         state_value = state_value or getattr(
             cls.context.new_class, cls.context.state_name, None
         )
@@ -114,11 +110,7 @@ class StateMachineMetaclass(type):
             raise ValueError(
                 "Empty state is disallowed, yet no initial state is given!"
             )
-        state_value = (
-            cls.context
-            .new_meta['translator']
-            .translate(state_value)
-        )
+        state_value = cls.context.new_meta["translator"].translate(state_value)
         cls.context.state_value = state_value
 
     @classmethod
@@ -130,28 +122,26 @@ class StateMachineMetaclass(type):
         """
         setattr(
             cls.context.new_class,
-            cls.context.new_meta['state_attribute_name'],
-            cls.context.state_value)
-        setattr(
-            cls.context.new_class,
-            cls.context.state_name,
-            utils.state_property)
+            cls.context.new_meta["state_attribute_name"],
+            cls.context.state_value,
+        )
+        setattr(cls.context.new_class, cls.context.state_name, utils.state_property)
 
-        setattr(cls.context.new_class, 'is_', utils.is_)
-        setattr(cls.context.new_class, 'can_be_', utils.can_be_)
-        setattr(cls.context.new_class, 'set_', utils.set_)
+        setattr(cls.context.new_class, "is_", utils.is_)
+        setattr(cls.context.new_class, "can_be_", utils.can_be_)
+        setattr(cls.context.new_class, "set_", utils.set_)
 
     @classmethod
     def _generate_standard_transitions(cls):
         """Generate methods used for transitions."""
-        allowed_transitions = cls.context.get_config('transitions', {})
+        allowed_transitions = cls.context.get_config("transitions", {})
         for key, transitions in allowed_transitions.items():
-            key = cls.context.new_meta['translator'].translate(key)
+            key = cls.context.new_meta["translator"].translate(key)
 
             new_transitions = set()
             for trans in transitions:
                 if not isinstance(trans, Enum):
-                    trans = cls.context.new_meta['translator'].translate(trans)
+                    trans = cls.context.new_meta["translator"].translate(trans)
                 new_transitions.add(trans)
 
             cls.context.new_transitions[key] = new_transitions
@@ -164,48 +154,49 @@ class StateMachineMetaclass(type):
     def _generate_standard_methods(cls):
         """Generate standard setters, getters and checkers."""
         for state in cls.context.states_enum:
-            getter_name = 'is_{name}'.format(name=state.value)
+            getter_name = "is_{name}".format(name=state.value)
             cls.context.new_methods[getter_name] = utils.generate_getter(state)
 
-            setter_name = 'set_{name}'.format(name=state.value)
+            setter_name = "set_{name}".format(name=state.value)
             cls.context.new_methods[setter_name] = utils.generate_setter(state)
 
-            checker_name = 'can_be_{name}'.format(name=state.value)
+            checker_name = "can_be_{name}".format(name=state.value)
             checker = utils.generate_checker(state)
             cls.context.new_methods[checker_name] = checker
 
-        cls.context.new_methods['actual_state'] = utils.actual_state
-        cls.context.new_methods['as_enum'] = utils.as_enum
-        cls.context.new_methods['force_set'] = utils.force_set
+        cls.context.new_methods["actual_state"] = utils.actual_state
+        cls.context.new_methods["as_enum"] = utils.as_enum
+        cls.context.new_methods["force_set"] = utils.force_set
 
     @classmethod
     def _generate_named_checkers(cls):
-        named_checkers = cls.context.get_config('named_checkers', None) or []
+        named_checkers = cls.context.get_config("named_checkers", None) or []
         for method, key in named_checkers:
             if method in cls.context.new_methods:
                 raise ValueError(
                     "Name collision for named checker '{checker}' - this "
-                    "name is reserved for other auto generated method."
-                    .format(checker=method)
+                    "name is reserved for other auto generated method.".format(
+                        checker=method
+                    )
                 )
 
-            key = cls.context.new_meta['translator'].translate(key)
+            key = cls.context.new_meta["translator"].translate(key)
             cls.context.new_methods[method] = utils.generate_checker(key.value)
 
     @classmethod
     def _generate_named_transitions(cls):
-        named_transitions = (
-            cls.context.get_config('named_transitions', None) or [])
+        named_transitions = cls.context.get_config("named_transitions", None) or []
 
-        translator = cls.context.new_meta['translator']
+        translator = cls.context.new_meta["translator"]
         for item in named_transitions:
             method, key, from_values = cls._unpack_named_transition_tuple(item)
 
             if method in cls.context.new_methods:
                 raise ValueError(
                     "Name collision for transition '{transition}' - this name "
-                    "is reserved for other auto generated method."
-                    .format(transition=method)
+                    "is reserved for other auto generated method.".format(
+                        transition=method
+                    )
                 )
 
             key = translator.translate(key)
@@ -221,7 +212,7 @@ class StateMachineMetaclass(type):
     def _unpack_named_transition_tuple(cls, item):
         try:
             method, key = item
-            from_values = cls.context['states_enum']
+            from_values = cls.context["states_enum"]
         except ValueError:
             method, key, from_values = item
             if from_values is None:
@@ -237,8 +228,7 @@ class StateMachineMetaclass(type):
         for name, method in cls.context.new_methods.items():
             if hasattr(cls.context.new_class, name):
                 raise ValueError(
-                    "Name collision in state machine class - '{name}'."
-                    .format(name)
+                    "Name collision in state machine class - '{name}'.".format(name)
                 )
 
             setattr(cls.context.new_class, name, method)
@@ -247,41 +237,40 @@ class StateMachineMetaclass(type):
     def _set_complete_option(cls):
         """Check and set complete option."""
         get_config = cls.context.get_config
-        complete = get_config('complete', None)
+        complete = get_config("complete", None)
         if complete is None:
             conditions = [
-                get_config('transitions', False),
-                get_config('named_transitions', False),
+                get_config("transitions", False),
+                get_config("named_transitions", False),
             ]
             complete = not any(conditions)
 
-        cls.context.new_meta['complete'] = complete
+        cls.context.new_meta["complete"] = complete
 
     @classmethod
     def _set_up_config_getter(cls):
-        meta = getattr(cls.context.new_class, 'Meta', DefaultMeta)
+        meta = getattr(cls.context.new_class, "Meta", DefaultMeta)
         cls.context.get_config = partial(get_config, meta)
 
     @classmethod
     def _set_up_translator(cls):
-        translator = utils.EnumValueTranslator(cls.context['states_enum'])
-        cls.context.new_meta['translator'] = translator
+        translator = utils.EnumValueTranslator(cls.context["states_enum"])
+        cls.context.new_meta["translator"] = translator
 
     @classmethod
     def _calculate_state_name(cls):
-        cls.context.state_name = 'state'
-        new_state_name = '_' + cls.context.state_name
-        cls.context.new_meta['state_attribute_name'] = new_state_name
+        cls.context.state_name = "state"
+        new_state_name = "_" + cls.context.state_name
+        cls.context.new_meta["state_attribute_name"] = new_state_name
 
     @classmethod
     def _complete_meta_for_new_class(cls):
-        cls.context.new_meta['transitions'] = cls.context.new_transitions
-        cls.context.new_meta['config_getter'] = cls.context['get_config']
-        setattr(cls.context.new_class, '_meta', cls.context['new_meta'])
+        cls.context.new_meta["transitions"] = cls.context.new_transitions
+        cls.context.new_meta["config_getter"] = cls.context["get_config"]
+        setattr(cls.context.new_class, "_meta", cls.context["new_meta"])
 
 
-class StateMachine(six.with_metaclass(StateMachineMetaclass)):
-
+class StateMachine(metaclass=StateMachineMetaclass):
     """State machine."""
 
 
